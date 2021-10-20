d716c44fe23b2c3f1731fb9523cb58f625f3c64a
Roshan Rao <89931020+roshanarao@users.noreply.github.com>
2021-10-20 13:02:46 -0400

    Add files via upload

---
+++ Tarp.java
@@ -0,0 +1,18 @@
+public class Tarp{
+	private double t;
+	private double l;
+	private ArrayList<Needle> needles;
+	
+	public Tarp(double t, double l){
+		
+		
+	}
+	
+	public int numberOfCrossings(){
+		
+	}
+	
+	
+	
+
+}
