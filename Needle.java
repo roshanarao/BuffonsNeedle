d32ea369e7e45993c9eb415ea3ee184c7c6b7f7f
rrao <rrao@friendsbalt.org>
2021-10-25 12:29:17 -0400

    yeah maybe i understand this

---
+++ Needle.java
@@ -0,0 +1,40 @@
+public class Needle{
+	private final double length;
+	private final double xPosition;
+	private final double yPosition;
+	private final double angle;
+	private final Tarp tarp;
+	
+	
+	public Needle(double length, double x, double y, double theta, Tarp t){
+		this.length = length;
+		this.xPosition = x;
+		this.yPosition = y;
+		this.angle = theta;
+		this.tarp = t;
+		
+	}
+	public Needle(double l, Tarp t){
+	//make a random needle of length L on the tarp
+	}
+	
+	public double center(){
+	//return location of the center
+	}
+	
+	public double leftTip(){
+	
+	}
+	
+	public double rightTip(){
+	
+	}
+	
+	public double angle(){
+	
+	}
+	
+	
+	
+	
+}
